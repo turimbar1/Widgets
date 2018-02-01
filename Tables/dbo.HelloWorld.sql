abf55c4f257e5337e34fefbc328afd42a760676a
unknown <andrew.pierce@RED-GATE>
2018-02-01 09:11:38 -0800

    Dropped column

--- Tables/dbo.HelloWorld.sql
+++ Tables/dbo.HelloWorld.sql
@@ -1,8 +1,7 @@
 CREATE TABLE [dbo].[HelloWorld]
 (
 [HelloId] [bigint] NOT NULL,
-[HelloDesc] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[HelloName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[HelloDesc] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[HelloWorld] ADD CONSTRAINT [PK__HelloWor__02FA286F20B6257F] PRIMARY KEY CLUSTERED  ([HelloId]) ON [PRIMARY]
