3fd1519eaa10a216c30a76b135f815183416c6ca
unknown <andrew.pierce@RED-GATE>
2018-02-02 14:17:45 -0800

    Adding a table

---
+++ Tables/dbo.ObjectTest.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[ObjectTest]
+(
+[ObjectID] [bigint] NOT NULL,
+[ObjectName] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ObjectTest] ADD CONSTRAINT [PK__ObjectTe__9A6192B18CF93115] PRIMARY KEY CLUSTERED  ([ObjectID]) ON [PRIMARY]
+GO
