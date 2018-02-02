2c83924e5d386c82af5f901924cf0de553b45663
unknown <andrew.pierce@RED-GATE>
2018-02-02 14:45:24 -0800

    added data and dropped an extra table

--- Tables/dbo.ObjectTest2.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[ObjectTest2]
-(
-[ObjectId] [bigint] NOT NULL,
-[ObjectDescription] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[ObjectTest2] ADD CONSTRAINT [PK__ObjectTe__9A619291260412F2] PRIMARY KEY CLUSTERED  ([ObjectId]) ON [PRIMARY]
-GO
