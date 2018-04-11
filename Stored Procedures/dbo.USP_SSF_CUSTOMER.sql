4482f39f770f1b1381dafaabcc0b642c216b2246
unknown <andrew.pierce@RED-GATE>
2018-04-11 13:26:27 -0700

    Add two tables and a Sproc

---
+++ Stored Procedures/dbo.USP_SSF_CUSTOMER.sql
@@ -0,0 +1,12 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[USP_SSF_CUSTOMER] AS
+BEGIN
+SELECT CustId
+     , CustFirstname
+     , CustLastname FROM dbo.Customer
+END
+
+GO
---
+++ Tables/dbo.NewTestObject.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[NewTestObject]
+(
+[objectId] [bigint] NOT NULL,
+[objectDescription] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[NewTestObject] ADD CONSTRAINT [PK__NewTestO__5243E26A30FCE0CA] PRIMARY KEY CLUSTERED  ([objectId]) ON [PRIMARY]
+GO
