312c76c11ce70235a6572223806bcbd9161eda8c
unknown <andrew.pierce@RED-GATE>
2018-06-26 10:30:38 -0700

    Added some data, a sproc, and a column

--- Tables/dbo.WidgetTest.sql
+++ Tables/dbo.WidgetTest.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[WidgetTest]
 (
 [WidgetId] [bigint] NOT NULL,
-[WidgetDesc] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[WidgetDesc] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[WidgetName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[WidgetTest] ADD CONSTRAINT [PK__WidgetTe__ADFD3012D69AD3E3] PRIMARY KEY CLUSTERED  ([WidgetId]) ON [PRIMARY]
