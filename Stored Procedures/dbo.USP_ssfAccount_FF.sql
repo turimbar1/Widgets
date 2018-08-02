73b2dfcb43be65822c79ae8184cbde74141e3285
unknown <andrew.pierce@RED-GATE>
2018-08-02 09:44:22 -0700

    Edited a table, sproc and added some data

--- Stored Procedures/dbo.USP_ssfAccount_FF.sql
+++ Stored Procedures/dbo.USP_ssfAccount_FF.sql
@@ -5,38 +5,27 @@ GO
 CREATE PROCEDURE [dbo].[USP_ssfAccount_FF]
 AS
 BEGIN
-    SELECT [﻿ID]
-         , ISDELETED
-         , MASTERRECORDID
-         , NAME
-         , TYPE
-         , RECORDTYPEID
-         , PARENTID
-         , BILLINGSTREET
-         , BILLINGCITY
-         , BILLINGSTATE
-         , BILLINGPOSTALCODE
-         , BILLINGCOUNTRY
-         , BILLINGSTATECODE
-         , BILLINGCOUNTRYCODE
-         , BILLINGLATITUDE
-         , BILLINGLONGITUDE
-         , BILLINGGEOCODEACCURACY
-         , BILLINGADDRESS
-         , PHONE
-         , FAX
-         , ACCOUNTNUMBER
-         , WEBSITE
-         , PHOTOURL
-         , INDUSTRY
-         , ANNUALREVENUE
-         , NUMBEROFEMPLOYEES
-         , OWNERSHIP
-         , TICKERSYMBOL
-         , DESCRIPTION
-         , CURRENCYISOCODE
-         , OWNERID
-    FROM dbo.Account_FF;
+    SELECT [﻿ID],
+           ISDELETED,
+           MASTERRECORDID,
+           NAME,
+           TYPE,
+           RECORDTYPEID,
+           PARENTID,
+           BILLINGSTREET,
+           BILLINGCITY,
+           BILLINGSTATE,
+           BILLINGPOSTALCODE,
+           BILLINGCOUNTRY,
+           FAX,
+           ACCOUNTNUMBER,
+           WEBSITE,
+           PHOTOURL,
+           INDUSTRY,
+           TICKERSYMBOL,
+           DESCRIPTION,
+           CURRENCYISOCODE,
+           OWNERID
+      FROM dbo.Account_FF;
 END;
-
 GO
--- Tables/dbo.Customer.sql
+++ Tables/dbo.Customer.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[Customer]
 [CustId] [bigint] NOT NULL,
 [CustFirstname] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CustLastname] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[CustAddress] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[CustAddress] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Last_Modified] [datetime] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Customer] ADD CONSTRAINT [PK__Customer__049E3AA9B86CDE0B] PRIMARY KEY CLUSTERED  ([CustId]) ON [PRIMARY]
