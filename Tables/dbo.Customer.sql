268b8ae19947512a675681f717703f3d716bb80c
unknown <andrew.pierce@RED-GATE>
2018-09-04 13:34:14 -0700

    Added a column a sproc and some data

--- Tables/dbo.Customer.sql
+++ Tables/dbo.Customer.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[Customer]
 [CustFirstname] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CustLastname] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CustAddress] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Last_Modified] [datetime] NULL
+[Last_Modified] [datetime] NULL,
+[LastModifiedBy] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Customer] ADD CONSTRAINT [PK__Customer__049E3AA9B86CDE0B] PRIMARY KEY CLUSTERED  ([CustId]) ON [PRIMARY]
