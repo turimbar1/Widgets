462c806f2b20358a0dcd8821435b10c1b5af8cab
unknown <andrew.pierce@RED-GATE>
2018-10-04 10:41:22 -0700

    Feature 110 - added a column and a sproc

---
+++ Stored Procedures/dbo.USP_GetProject.sql
@@ -0,0 +1,10 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[USP_GetProject] AS
+BEGIN
+SELECT projectID, projectDescription, projectName FROM dbo.project;
+END
+
+GO
