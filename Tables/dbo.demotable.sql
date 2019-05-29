8fa7eecb4bca5ca793da03f3a071645b6a6b9e63
Andrew Pierce <andrew.pierce@red-gate.com>
2019-05-29 08:58:52 -0700

    Edited some reference data, added a column and a sproc

--- Tables/dbo.demotable.sql
+++ Tables/dbo.demotable.sql
@@ -1,20 +1,10 @@
 CREATE TABLE [dbo].[demotable]
 (
-[DemoId] [bigint] NOT NULL,
-[DemoDescription] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[FirstName] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[LastName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoDate] [datetime] NULL,
-[DemoAddress] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[ModifiedDate] [date] NULL,
-[LastModifiedBy] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoAddress2] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoNotes] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoFeedback] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoUser] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoNotes2] [nvarchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[LinkedInID] [int] NULL
+[TableId] [bigint] NOT NULL,
+[TableName] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[TableDesc] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DemoNotes] [nvarchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[demotable] ADD CONSTRAINT [PK__demotabl__977B80755E2EBDD1] PRIMARY KEY CLUSTERED  ([DemoId]) ON [PRIMARY]
+ALTER TABLE [dbo].[demotable] ADD CONSTRAINT [PK__demotabl__7D5F01EE9AAD425A] PRIMARY KEY CLUSTERED  ([TableId]) ON [PRIMARY]
 GO
