dff206edbaf93aeb1ab70ec7c93bc2b9079f12d2
Andrew Pierce <andrew.pierce@red-gate.com>
2019-06-06 15:13:41 -0700

    Added Linked In ID - not sure where the RedgatID column came from

--- Tables/dbo.DemoTest2.sql
+++ Tables/dbo.DemoTest2.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[DemoTest2]
 (
 [DemoId] [bigint] NOT NULL,
 [DemoDescrip] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoDate] [datetime2] NULL
+[RedgateId] [int] NULL,
+[LinkedInId] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[DemoTest2] ADD CONSTRAINT [PK__DemoTest__977B80751365E757] PRIMARY KEY CLUSTERED  ([DemoId]) ON [PRIMARY]
