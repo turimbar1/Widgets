a44be701a8123accff23e4f6e96ce315f36c369d
Andrew Pierce <andrew.pierce@red-gate.com>
2019-07-25 10:30:29 -0700

    DSC-233

--- Tables/dbo.DemoTest2.sql
+++ Tables/dbo.DemoTest2.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[DemoTest2]
 [DemoId] [bigint] NOT NULL,
 [DemoDescrip] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [RedgateId] [int] NULL,
-[LinkedInId] [int] NULL
+[LinkedInId] [int] NULL,
+[RedgateEmail] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[DemoTest2] ADD CONSTRAINT [PK__DemoTest__977B80751365E757] PRIMARY KEY CLUSTERED  ([DemoId]) ON [PRIMARY]
