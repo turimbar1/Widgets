6adf72c1356542c33e7c2f8689946ea71a50b1e9
Andrew Pierce <andrew.pierce@red-gate.com>
2019-10-18 10:45:45 -0700

    Edited for syntax

--- Stored Procedures/dbo.USP_GetProject.sql
+++ Stored Procedures/dbo.USP_GetProject.sql
@@ -2,13 +2,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-<<<<<<< HEAD
-CREATE PROCEDURE [dbo].[USP_GetProject] AS
-BEGIN
-SELECT projectID, projectDescription, projectOwner, projectItem, projectName, lastModified
-FROM dbo.project;
-END
-=======
+
 
 CREATE PROCEDURE [dbo].[USP_GetProject] AS
 BEGIN
@@ -16,5 +10,4 @@ SELECT projectID, projectDescription
 FROM dbo.project;
 END
 
->>>>>>> Feature
 GO
