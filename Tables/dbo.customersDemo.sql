17be7d8e2fae63e2c904be46df90ba594229e86f
Andrew Pierce <andrew.pierce@red-gate.com>
2020-01-15 12:46:46 -0800

    Added a column

--- Tables/dbo.customersDemo.sql
+++ Tables/dbo.customersDemo.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[customersDemo]
 [CustDesc] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CustName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CustDOB] [date] NULL,
-[EmailAddress] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[EmailAddress] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AzureEnvironment] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[customersDemo] ADD CONSTRAINT [PK__customer__049E3A89295CB0A4] PRIMARY KEY CLUSTERED  ([CustID]) ON [PRIMARY]
