8917ff566770996ac6ecc6bacc7342b0ef4b9ced
Andrew Pierce <andrew.pierce@red-gate.com>
2020-01-15 13:24:35 -0800

    Edited some objects

--- Tables/dbo.customersDemo.sql
+++ Tables/dbo.customersDemo.sql
@@ -5,7 +5,8 @@ CREATE TABLE [dbo].[customersDemo]
 [CustName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CustDOB] [date] NULL,
 [EmailAddress] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[AzureEnvironment] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[AzureEnvironment] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AzureEnvironmentID] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[customersDemo] ADD CONSTRAINT [PK__customer__049E3A89295CB0A4] PRIMARY KEY CLUSTERED  ([CustID]) ON [PRIMARY]
