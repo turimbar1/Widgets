f88d4f8bdeccad61a6a67293ca788973a41f8d2f
Andrew Pierce <andrew.pierce@red-gate.com>
2020-01-31 09:35:07 -0800

    edited some data, added a column and added test proc

--- Tables/dbo.demotable.sql
+++ Tables/dbo.demotable.sql
@@ -13,7 +13,8 @@ CREATE TABLE [dbo].[demotable]
 [DemoFeedback] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [TableDescription] [nvarchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [FacebookUsername] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[AzurePassword] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[AzurePassword] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AzureAccountName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[demotable] ADD CONSTRAINT [PK__demotabl__977B80755E2EBDD1] PRIMARY KEY CLUSTERED  ([DemoId]) ON [PRIMARY]
