a8c71c33cb0388f64e4007f1d28911fce5c4967b
Andrew Pierce <andrew.pierce@red-gate.com>
2020-02-20 12:50:54 -0800

    #A5 Added RedgateId column, a new sproc, edited some reference data

--- Tables/dbo.customersDemo.sql
+++ Tables/dbo.customersDemo.sql
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[customersDemo]
 [AzureEnvironment] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [AzureEnvironmentID] [int] NULL,
 [AzureUsername] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[AzureEnvironmentName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[AzureEnvironmentName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[RedgateID] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[customersDemo] ADD CONSTRAINT [PK__customer__049E3A89295CB0A4] PRIMARY KEY CLUSTERED  ([CustID]) ON [PRIMARY]
