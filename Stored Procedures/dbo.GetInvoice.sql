f8fe50c45c731d6226d64477d337cffa263de9c8
Andrew Pierce <andrew.pierce@red-gate.com>
2020-03-13 12:51:04 -0700

    altered some records - a proc and a table

--- Stored Procedures/dbo.GetInvoice.sql
+++ Stored Procedures/dbo.GetInvoice.sql
@@ -2,9 +2,12 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE [dbo].[GetInvoice] AS
+CREATE PROCEDURE [dbo].[GetInvoice]
+AS
 BEGIN
-SELECT invoice_number, invoice_date, invoice_customer_id FROM dbo.DM_INVOICE
-END
+    SELECT invoice_number,
+           invoice_date
+      FROM dbo.DM_INVOICE;
+END;
 
 GO
