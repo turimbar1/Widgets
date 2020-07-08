a4d7132b1b3dc9e8825cfac258cccd610661ca2a
Andrew Pierce <andrew.pierce@red-gate.com>
2020-07-08 12:29:29 -0700

    rename column

--- Tables/dbo.DemoTest2.sql
+++ Tables/dbo.DemoTest2.sql
@@ -6,7 +6,7 @@ CREATE TABLE [dbo].[DemoTest2]
 [LinkedInId] [int] NULL,
 [RedgateEmail] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [AzureID] [int] NULL,
-[AzureUser] [int] NULL
+[AzureUserNew] [int] NULL
 )
 GO
 ALTER TABLE [dbo].[DemoTest2] ADD CONSTRAINT [PK__DemoTest__977B80751365E757] PRIMARY KEY CLUSTERED  ([DemoId])
