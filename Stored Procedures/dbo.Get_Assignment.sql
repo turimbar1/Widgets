6ba843ad11c6ad3eabe664a31f70a329730d71cc
Andrew Pierce <andrew.pierce@red-gate.com>
2020-07-16 12:36:33 -0700

    added a proc, column and edited some data

---
+++ Stored Procedures/dbo.Get_Assignment.sql
@@ -0,0 +1,17 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[Get_Assignment] AS
+BEGIN
+SELECT
+    assignment_id,
+    person_id,
+    emp_id,
+    emp_jobtitle,
+    assignment_notes
+FROM
+    dbo.DM_ASSIGNMENT;
+END
+
+GO
--- Tables/dbo.HelloDemo.sql
+++ Tables/dbo.HelloDemo.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[HelloDemo]
 (
 [DemoId] [bigint] NOT NULL,
 [DemoFName] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoDescription] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[DemoDescription] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AzureUser] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[HelloDemo] ADD CONSTRAINT [PK__HelloDem__977B8075C276CD8C] PRIMARY KEY CLUSTERED  ([DemoId]) ON [PRIMARY]
