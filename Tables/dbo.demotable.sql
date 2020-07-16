b06e2234f7e6977b6cf726ffa3e5897e0da82bbe
Andrew Pierce <andrew.pierce@red-gate.com>
2020-07-16 13:22:49 -0700

    adding col

--- Tables/dbo.demotable.sql
+++ Tables/dbo.demotable.sql
@@ -18,7 +18,8 @@ CREATE TABLE [dbo].[demotable]
 [NikeID] [int] NULL,
 [demoDate2] [datetime2] NULL,
 [AzureUserDescription] [nvarchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DemoName] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[DemoName] [nvarchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DemoName2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[demotable] ADD CONSTRAINT [PK__demotabl__977B80755E2EBDD1] PRIMARY KEY CLUSTERED  ([DemoId]) ON [PRIMARY]
